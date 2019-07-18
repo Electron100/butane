b73c4d9474207166496a652bdd3696bd6830fe27
James Oakley <james@electronstudio.org>
2019-07-17 21:50:10 -0400

    More ForeignKey abilities

--- propane_core/src/fkey.rs
+++ propane_core/src/fkey.rs
@@ -1,14 +1,22 @@
 use crate::*;
 use once_cell::unsync::OnceCell;
+use std::fmt::{Debug, Formatter};
 
+/// Initialize using `From` or `from_pk`
 pub struct ForeignKey<T>
 where
     T: DBObject,
 {
+    // At least one must be initialized, but both need not be
     val: OnceCell<T>,
     valpk: OnceCell<SqlVal>,
 }
 impl<T: DBObject> ForeignKey<T> {
+    pub fn from_pk(pk: T::PKType) -> Self {
+        let ret = Self::new_raw();
+        ret.valpk.set(pk.into_sql()).unwrap();
+        ret
+    }
     /// Returns a reference to the value. It must have already been loaded. If not, returns Error::ValueNotLoaded
     pub fn get(&self) -> Result<&T> {
         self.val.get().ok_or(Error::ValueNotLoaded.into())
@@ -22,6 +30,59 @@ impl<T: DBObject> ForeignKey<T> {
             T::get(conn, T::PKType::from_sql(pk)?)
         })
     }
+
+    fn new_raw() -> Self {
+        ForeignKey {
+            val: OnceCell::new(),
+            valpk: OnceCell::new(),
+        }
+    }
+
+    fn ensure_valpk(&self) -> &SqlVal {
+        match self.valpk.get() {
+            Some(sqlval) => (),
+            None => match self.val.get() {
+                Some(val) => self.valpk.set(val.pk().to_sql()).unwrap(),
+                None => panic!("Invalid foreign key state"),
+            },
+        }
+        &self.valpk.get().unwrap()
+    }
+}
+
+impl<T: DBObject> From<T> for ForeignKey<T> {
+    fn from(obj: T) -> Self {
+        let ret = Self::new_raw();
+        ret.val.set(obj).ok();
+        ret
+    }
+}
+impl<T: DBObject> From<&T> for ForeignKey<T> {
+    fn from(obj: &T) -> Self {
+        Self::from_pk(obj.pk().clone())
+    }
+}
+impl<T: DBObject> Clone for ForeignKey<T> {
+    fn clone(&self) -> Self {
+        // Once specialization lands, it would be nice to clone val if
+        // it's cloneable. Then we wouldn't have to ensure the pk
+        self.ensure_valpk();
+        ForeignKey {
+            val: OnceCell::new(),
+            valpk: self.valpk.clone(),
+        }
+    }
+}
+impl<T: DBObject> PartialEq<ForeignKey<T>> for ForeignKey<T> {
+    fn eq(&self, other: &ForeignKey<T>) -> bool {
+        self.ensure_valpk().eq(other.ensure_valpk())
+    }
+}
+impl<T: DBObject> Eq for ForeignKey<T> {}
+impl<T: DBObject> Debug for ForeignKey<T> {
+    fn fmt(&self, f: &mut Formatter) -> std::fmt::Result {
+        self.ensure_valpk().fmt(f)
+    }
 }
 
 impl<T> ToSql for ForeignKey<T>
@@ -30,13 +91,7 @@ where
 {
     const SQLTYPE: SqlType = <T as DBObject>::PKType::SQLTYPE;
     fn into_sql(self) -> SqlVal {
-        match self.valpk.get() {
-            Some(sqlval) => sqlval.clone(),
-            None => match self.val.get() {
-                Some(val) => val.pk().to_sql(),
-                None => panic!("Invalid foreign key state"),
-            },
-        }
+        self.ensure_valpk().clone()
     }
 }
 impl<T> FromSql for ForeignKey<T>
@@ -48,15 +103,5 @@ where
             valpk: OnceCell::from(val),
             val: OnceCell::new(),
         })
-    }
-}
-impl<T: DBObject> Clone for ForeignKey<T> {
-    fn clone(&self) -> Self {
-        // Once specialization lands, it would be nice to clone val if
-        // it's cloneable
-        ForeignKey {
-            val: OnceCell::new(),
-            valpk: self.valpk.clone(),
-        }
     }
 }

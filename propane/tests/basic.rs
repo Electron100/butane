1084a25a3db398ec945977e4999a62da90e29151
James Oakley <james@electronstudio.org>
2019-07-18 21:52:35 -0400

    Common tests module

--- propane/tests/basic.rs
+++ propane/tests/basic.rs
@@ -2,7 +2,9 @@ use paste;
 use propane::db::{Connection, ConnectionSpec};
 use propane::model;
 use propane::prelude::*;
-use propane::{migrations::Migration, ForeignKey};
+use propane::ForeignKey;
+
+mod common;
 
 #[model]
 #[derive(PartialEq, Eq, Debug, Clone)]
@@ -35,52 +37,6 @@ impl Bar {
             foo: foo.into(),
         }
     }
-}
-
-fn setup_db(spec: &ConnectionSpec) {
-    let mut root = std::env::current_dir().unwrap();
-    root.push("propane/migrations");
-    dbg!(&root);
-    let migrations = propane::migrations::from_root(root);
-    let current = migrations.get_current();
-    let backend = propane::db::get_backend(&spec.backend_name)
-        .expect(&format!("couldn't get db backend '{}'", &spec.backend_name));
-    // todo using different migration names to avoid race is hacky, should find way to use different
-    // migration directory for each test.
-    let initial: Migration = migrations
-        .create_migration_sql(backend, &format!("init_{}", spec.conn_str), None, &current)
-        .expect("expected to create migration without error")
-        .expect("expected non-None migration");
-    let sql = initial.get_up_sql(&spec.backend_name).unwrap();
-    let conn = propane::db::connect(spec).unwrap();
-    conn.execute(&sql).unwrap();
-}
-
-fn reset_db(connstr: String) {
-    std::fs::remove_file(connstr).ok();
-}
-
-macro_rules! maketest {
-    ($fname:ident, $backend:expr, $connstr:expr) => {
-        paste::item! {
-            #[test]
-            pub fn [<$fname _ $backend>]() {
-                dbg!($connstr);
-                reset_db($connstr);
-                let spec = ConnectionSpec::new(stringify!($backend), $connstr);
-                setup_db(&spec);
-                let conn = propane::db::connect(&spec).unwrap();
-                $fname(conn);
-                //reset_db($connstr);
-            }
-        }
-    };
-}
-
-macro_rules! testall {
-    ($fname:ident) => {
-        maketest!($fname, sqlite, format!("test_{}.db", stringify!($fname)));
-    };
 }
 
 fn basic_crud(conn: Connection) {
@@ -126,7 +82,7 @@ fn foreign_key(conn: Connection) {
     let mut bar = Bar::new("tarzan", foo.clone());
     bar.save(&conn).unwrap();
     let bar2 = Bar::get(&conn, "tarzan".to_string()).unwrap();
-    
+
     let foo2: &Foo = bar2.foo.load(&conn).unwrap();
     assert_eq!(&foo, foo2);
 

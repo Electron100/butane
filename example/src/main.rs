556c8f560b1303364b837f83cca4b1aa30fdcf2a
James Oakley <james@electronstudio.org>
2020-04-05 17:50:29 -0400

    Warnings in example

--- example/src/main.rs
+++ example/src/main.rs
@@ -1,3 +1,4 @@
+#![allow(dead_code)]
 use failure;
 use propane::db::{Connection, ConnectionSpec};
 use propane::model;
@@ -36,15 +37,15 @@ struct Tag {
 fn query() -> Result<()> {
     let conn = establish_connection()?;
     let _specific_post = Post::get(&conn, 1);
-    let published_posts = query!(Post, published == true).limit(5).load(&conn)?;
+    let _published_posts = query!(Post, published == true).limit(5).load(&conn)?;
     let unliked_posts = query!(Post, published == true && likes < 5).load(&conn)?;
-    let blog: &Blog = unliked_posts.first().unwrap().blog.load(&conn)?;
-    let tagged_posts = query!(Post, tags.contains("dinosaurs")).load(&conn);
+    let _blog: &Blog = unliked_posts.first().unwrap().blog.load(&conn)?;
+    let _tagged_posts = query!(Post, tags.contains("dinosaurs")).load(&conn);
     //let tagged_posts2 = query!(Post, tags.contains(tag = "dinosaurs")).load(&conn);
     let blog: Blog = find!(Blog, name == "Bears", &conn).unwrap();
-    let posts_in_blog = query!(Post, blog == { &blog }).load(&conn);
-    let posts_in_blog2 = query!(Post, blog == { blog }).load(&conn);
-    let posts_in_blog = query!(Post, blog.matches(name == "Bears")).load(&conn);
+    let _posts_in_blog = query!(Post, blog == { &blog }).load(&conn);
+    let _posts_in_blog2 = query!(Post, blog == { blog }).load(&conn);
+    let _posts_in_blog = query!(Post, blog.matches(name == "Bears")).load(&conn);
     Ok(())
 }
 

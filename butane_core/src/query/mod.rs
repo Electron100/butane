713335fb128111ed9ffb4333eec0eb9083dd248f
James Oakley <james@electronstudio.org>
2021-02-07 16:40:24 -0500

    Fix notes and docs

--- butane_core/src/query/mod.rs
+++ butane_core/src/query/mod.rs
@@ -148,7 +148,7 @@ impl<T: DataResult> Query<T> {
 
     /// Order the query results by the given column. Multiple calls to
     /// this method may be made, with earlier calls taking precedence.
-    /// It is recommended to use the [`colname!`](butane::colname)
+    /// It is recommended to use the `colname!`
     /// macro to construct the column name in a typesafe manner.
     pub fn order(mut self, column: &'static str, direction: OrderDirection) -> Query<T> {
         self.sort.push(Order { direction, column });

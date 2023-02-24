ed0a371f083f7370f1143041ed917db269d38888
John Vandenberg <jayvdb@gmail.com>
2023-02-24 10:42:50 +0800

    Upgrade clap to 4.1

--- butane_cli/src/main.rs
+++ butane_cli/src/main.rs
@@ -13,73 +13,73 @@ use std::path::PathBuf;
 type Result<T> = std::result::Result<T, anyhow::Error>;
 
 fn main() {
-    let app = clap::App::new("butane")
+    let app = clap::Command::new("butane")
         .version(env!("CARGO_PKG_VERSION"))
         .author("James Oakley <james@electronstudio.org>")
         .about("Manages butane database migrations")
         .max_term_width(80)
         .subcommand(
-            clap::SubCommand::with_name("init")
+            clap::Command::new("init")
                 .about("Initialize the database")
                 .arg(
-                    Arg::with_name("BACKEND")
+                    Arg::new("BACKEND")
                         .required(true)
                         .index(1)
                         .help("Database backend to use. 'sqlite' or 'pg'"),
                 )
                 .arg(
-                    Arg::with_name("CONNECTION")
+                    Arg::new("CONNECTION")
                         .required(true)
                         .index(2)
                         .help("Database connection string. Format depends on backend"),
                 ),
         )
         .subcommand(
-            clap::SubCommand::with_name("makemigration")
+            clap::Command::new("makemigration")
                 .about("Create a new migration")
                 .arg(
-                    Arg::with_name("NAME")
+                    Arg::new("NAME")
                         .required(true)
                         .index(1)
                         .help("Name to use for the migration"),
                 ),
         )
-        .subcommand(clap::SubCommand::with_name("migrate").about("Apply migrations"))
-        .subcommand(clap::SubCommand::with_name("list").about("List migrations"))
-				.subcommand(clap::SubCommand::with_name("collapse").about("Replace all migrations with a single migration representing the current model state.").arg(
-                    Arg::with_name("NAME")
+        .subcommand(clap::Command::new("migrate").about("Apply migrations"))
+        .subcommand(clap::Command::new("list").about("List migrations"))
+				.subcommand(clap::Command::new("collapse").about("Replace all migrations with a single migration representing the current model state.").arg(
+                    Arg::new("NAME")
                         .required(true)
                         .index(1)
                         .help("Name to use for the new migration"),
                 ))
         .subcommand(
-            clap::SubCommand::with_name("embed").about("Embed migrations in the source code"),
+            clap::Command::new("embed").about("Embed migrations in the source code"),
         )
         .subcommand(
-            clap::SubCommand::with_name("rollback")
+            clap::Command::new("rollback")
                 .about("Rollback migrations. With no arguments, undoes the latest migration. If the name of a migration is specified, rolls back until that migration is the latest applied migration")
                 .arg(
-                    Arg::with_name("NAME")
+                    Arg::new("NAME")
                         .required(false)
                         .index(1)
                         .help("Migration to roll back to"),
                 ),
         )
         .subcommand(
-						clap::SubCommand::with_name("clear")
-								.setting(clap::AppSettings::ArgRequiredElseHelp)
+						clap::Command::new("clear")
+								.arg_required_else_help(true)
 								.about("Clear data")
-								.subcommand(clap::SubCommand::with_name("data")
+								.subcommand(clap::Command::new("data")
 														.about("Clear all data from the database. The schema is left intact, but all instances of all models (i.e. all rows of all tables defined by the models) are deleted")))
         .subcommand(
-            clap::SubCommand::with_name("delete")
+            clap::Command::new("delete")
                 .about("Delete a table")
-                .setting(clap::AppSettings::ArgRequiredElseHelp)
+                .arg_required_else_help(true)
                 .subcommand(
-                    clap::SubCommand::with_name("table")
+                    clap::Command::new("table")
                         .about("Delete a table. Deleting a model in code does not currently lead to deletion of the table.")
                         .arg(
-                            Arg::with_name("TABLE")
+                            Arg::new("TABLE")
                                 .required(true)
                                 .index(1)
                                 .help("Name of table to delete"),
@@ -87,32 +87,31 @@ fn main() {
                 ),
         )
 				.subcommand(
-						clap::SubCommand::with_name("clean")
+						clap::Command::new("clean")
 								.about("Clean current migration state. Deletes the current migration working state which is generated on each build. This can be used as a workaround to remove stale tables from the schema, as Butane does not currently auto-detect model removals. The next build will recreate with only tables for the extant models."))
-        .setting(clap::AppSettings::ArgRequiredElseHelp);
+                                .arg_required_else_help(true);
     let args = app.get_matches();
     match args.subcommand() {
-        ("init", sub_args) => handle_error(init(sub_args)),
-        ("makemigration", sub_args) => handle_error(make_migration(sub_args)),
-        ("migrate", _) => handle_error(migrate()),
-        ("rollback", sub_args) => handle_error(rollback(sub_args)),
-        ("embed", _) => handle_error(embed()),
-        ("list", _) => handle_error(list_migrations()),
-        ("collapse", Some(sub_args)) => {
-            handle_error(collapse_migrations(sub_args.value_of("NAME")))
-        }
-        ("clear", Some(sub_args)) => match sub_args.subcommand() {
-            ("data", Some(_)) => handle_error(clear_data()),
-            (_, _) => eprintln!("Unknown clear command. Try: clear data"),
+        Some(("init", sub_args)) => handle_error(init(Some(sub_args))),
+        Some(("makemigration", sub_args)) => handle_error(make_migration(Some(sub_args))),
+        Some(("migrate", _)) => handle_error(migrate()),
+        Some(("rollback", sub_args)) => handle_error(rollback(Some(sub_args))),
+        Some(("embed", _)) => handle_error(embed()),
+        Some(("list", _)) => handle_error(list_migrations()),
+        Some(("collapse", sub_args)) => handle_error(collapse_migrations(sub_args.get_one("NAME"))),
+        Some(("clear", sub_args)) => match sub_args.subcommand() {
+            Some(("data", _)) => handle_error(clear_data()),
+            _ => eprintln!("Unknown clear command. Try: clear data"),
         },
-        ("delete", Some(sub_args)) => match sub_args.subcommand() {
-            ("table", Some(sub_args2)) => {
-                handle_error(delete_table(sub_args2.value_of("TABLE").unwrap()))
+        Some(("delete", sub_args)) => match sub_args.subcommand() {
+            Some(("table", sub_args2)) => {
+                handle_error(delete_table(sub_args2.get_one::<&str>("TABLE").unwrap()))
             }
-            (_, _) => eprintln!("Unknown delete command. Try: delete table"),
+            _ => eprintln!("Unknown delete command. Try: delete table"),
         },
-        ("clean", _) => handle_error(clean()),
-        (cmd, _) => eprintln!("Unknown command {cmd}"),
+        Some(("clean", _)) => handle_error(clean()),
+        Some((cmd, _)) => eprintln!("Unknown command {cmd}"),
+        None => eprintln!("Unknown command"),
     }
 }
 
@@ -144,8 +143,8 @@ fn default_name() -> String {
 
 fn init(args: Option<&ArgMatches>) -> Result<()> {
     let args = args.unwrap();
-    let name = args.value_of("BACKEND").unwrap();
-    let connstr = args.value_of("CONNECTION").unwrap();
+    let name: &String = args.get_one("BACKEND").unwrap();
+    let connstr: &String = args.get_one("CONNECTION").unwrap();
     if db::get_backend(name).is_none() {
         eprintln!("Unknown backend {name}");
         std::process::exit(1);
@@ -160,7 +159,7 @@ fn init(args: Option<&ArgMatches>) -> Result<()> {
 }
 
 fn make_migration(args: Option<&ArgMatches>) -> Result<()> {
-    let name_arg = args.and_then(|a| a.value_of("NAME"));
+    let name_arg = args.and_then(|a| a.get_one::<String>("NAME"));
     let name = match name_arg {
         Some(name) => format!("{}_{}", default_name(), name),
         None => default_name(),
@@ -202,7 +201,7 @@ fn rollback(args: Option<&ArgMatches>) -> Result<()> {
     let spec = load_connspec()?;
     let conn = db::connect(&spec)?;
 
-    match args.and_then(|a| a.value_of("NAME")) {
+    match args.and_then(|a| a.get_one::<String>("NAME")) {
         Some(to) => rollback_to(conn, to),
         None => rollback_latest(conn),
     }
@@ -305,7 +304,7 @@ fn list_migrations() -> Result<()> {
     Ok(())
 }
 
-fn collapse_migrations(new_initial_name: Option<&str>) -> Result<()> {
+fn collapse_migrations(new_initial_name: Option<&String>) -> Result<()> {
     let name = match new_initial_name {
         Some(name) => format!("{}_{}", default_name(), name),
         None => default_name(),

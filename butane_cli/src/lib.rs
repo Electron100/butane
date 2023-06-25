b80b5e2aba3a8e2943067d77d99678be26f3ff42
John Vandenberg <jayvdb@gmail.com>
2023-06-25 17:39:54 +0800

    Choose cli base directory once

--- butane_cli/src/lib.rs
+++ butane_cli/src/lib.rs
@@ -6,7 +6,11 @@
 //! breakages in the future.
 //! Backwards compatibility of the library will not even be considered, as the
 //! only objective of the crate is to provide a stable CLI.
-use std::{fs::File, io::Write, path::PathBuf};
+use std::{
+    fs::File,
+    io::Write,
+    path::{Path, PathBuf},
+};
 
 use butane::migrations::{
     copy_migration, FsMigrations, MemMigrations, Migration, MigrationMut, Migrations, MigrationsMut,
@@ -23,8 +27,8 @@ pub struct CliState {
     embedded: bool,
 }
 impl CliState {
-    pub fn load() -> Result<Self> {
-        let path = base_dir()?.join("clistate.json");
+    pub fn load(base_dir: &Path) -> Result<Self> {
+        let path = base_dir.join("clistate.json");
         let file = File::open(path);
         match file {
             Ok(file) => Ok(serde_json::from_reader(file)?),
@@ -32,8 +36,8 @@ impl CliState {
         }
     }
 
-    pub fn save(&self) -> Result<()> {
-        let path = base_dir()?.join("clistate.json");
+    pub fn save(&self, base_dir: &Path) -> Result<()> {
+        let path = base_dir.join("clistate.json");
         let file = File::create(path)?;
         serde_json::to_writer(file, &self)?;
         Ok(())
@@ -44,7 +48,7 @@ pub fn default_name() -> String {
     Utc::now().format("%Y%m%d_%H%M%S%3f").to_string()
 }
 
-pub fn init(name: &str, connstr: &str) -> Result<()> {
+pub fn init(base_dir: &PathBuf, name: &str, connstr: &str) -> Result<()> {
     if db::get_backend(name).is_none() {
         eprintln!("Unknown backend {name}");
         std::process::exit(1);
@@ -52,30 +56,30 @@ pub fn init(name: &str, connstr: &str) -> Result<()> {
 
     let spec = db::ConnectionSpec::new(name, connstr);
     db::connect(&spec)?; // ensure we can
-    std::fs::create_dir_all(base_dir()?)?;
-    spec.save(&base_dir()?)?;
+    std::fs::create_dir_all(base_dir)?;
+    spec.save(base_dir)?;
 
     Ok(())
 }
 
-pub fn make_migration(name: Option<&String>) -> Result<()> {
+pub fn make_migration(base_dir: &PathBuf, name: Option<&String>) -> Result<()> {
     let name = match name {
         Some(name) => format!("{}_{}", default_name(), name),
         None => default_name(),
     };
-    let mut ms = get_migrations()?;
+    let mut ms = get_migrations(base_dir)?;
     if ms.all_migrations()?.iter().any(|m| m.name() == name) {
         eprintln!("Migration {name} already exists");
         std::process::exit(1);
     }
-    let spec = load_connspec()?;
+    let spec = load_connspec(base_dir)?;
     let backend = spec.get_backend()?;
     let created = ms.create_migration(&backend, &name, ms.latest().as_ref())?;
     if created {
-        let cli_state = CliState::load()?;
+        let cli_state = CliState::load(base_dir)?;
         if cli_state.embedded {
             // Better include the new migration in the embedding
-            embed()?;
+            embed(base_dir)?;
         }
         println!("Created migration {name}");
     } else {
@@ -84,10 +88,10 @@ pub fn make_migration(name: Option<&String>) -> Result<()> {
     Ok(())
 }
 
-pub fn migrate() -> Result<()> {
-    let spec = load_connspec()?;
+pub fn migrate(base_dir: &PathBuf) -> Result<()> {
+    let spec = load_connspec(base_dir)?;
     let mut conn = db::connect(&spec)?;
-    let to_apply = get_migrations()?.unapplied_migrations(&conn)?;
+    let to_apply = get_migrations(base_dir)?.unapplied_migrations(&conn)?;
     println!("{} migrations to apply", to_apply.len());
     for m in to_apply {
         println!("Applying migration {}", m.name());
@@ -96,8 +100,8 @@ pub fn migrate() -> Result<()> {
     Ok(())
 }
 
-pub fn rollback_to(mut conn: Connection, to: &str) -> Result<()> {
-    let ms = get_migrations()?;
+pub fn rollback_to(base_dir: &Path, mut conn: Connection, to: &str) -> Result<()> {
+    let ms = get_migrations(base_dir)?;
     let to_migration = match ms.get_migration(to) {
         Some(m) => m,
         None => {
@@ -117,8 +121,8 @@ pub fn rollback_to(mut conn: Connection, to: &str) -> Result<()> {
     Ok(())
 }
 
-pub fn rollback_latest(mut conn: Connection) -> Result<()> {
-    match get_migrations()?.latest() {
+pub fn rollback_latest(base_dir: &Path, mut conn: Connection) -> Result<()> {
+    match get_migrations(base_dir)?.latest() {
         Some(m) => {
             println!("Rolling back migration  {}", m.name());
             m.downgrade(&mut conn)?;
@@ -131,8 +135,8 @@ pub fn rollback_latest(mut conn: Connection) -> Result<()> {
     Ok(())
 }
 
-pub fn embed() -> Result<()> {
-    let srcdir = std::env::current_dir()?.join("src");
+pub fn embed(base_dir: &Path) -> Result<()> {
+    let srcdir = base_dir.join("src");
     if !srcdir.exists() {
         eprintln!("src directory not found");
         std::process::exit(1);
@@ -140,7 +144,7 @@ pub fn embed() -> Result<()> {
     let path = srcdir.join("butane_migrations.rs");
 
     let mut mem_ms = MemMigrations::new();
-    for m in get_migrations()?.all_migrations()? {
+    for m in get_migrations(base_dir)?.all_migrations()? {
         let mut new_m = mem_ms.new_migration(&m.name());
         copy_migration(&m, &mut new_m)?;
         mem_ms.add_migration(new_m)?;
@@ -160,14 +164,14 @@ pub fn get_migrations() -> Result<MemMigrations, butane::Error> {{
     let mut f = std::fs::File::create(path)?;
     f.write_all(src.as_bytes())?;
 
-    let mut cli_state = CliState::load()?;
+    let mut cli_state = CliState::load(base_dir)?;
     cli_state.embedded = true;
-    cli_state.save()?;
+    cli_state.save(base_dir)?;
     Ok(())
 }
 
-pub fn load_connspec() -> Result<db::ConnectionSpec> {
-    match db::ConnectionSpec::load(base_dir()?) {
+pub fn load_connspec(base_dir: &PathBuf) -> Result<db::ConnectionSpec> {
+    match db::ConnectionSpec::load(base_dir) {
         Ok(spec) => Ok(spec),
         Err(butane::Error::IO(_)) => {
             eprintln!("No Butane connection info found. Did you run butane init?");
@@ -177,10 +181,10 @@ pub fn load_connspec() -> Result<db::ConnectionSpec> {
     }
 }
 
-pub fn list_migrations() -> Result<()> {
-    let spec = load_connspec()?;
+pub fn list_migrations(base_dir: &PathBuf) -> Result<()> {
+    let spec = load_connspec(base_dir)?;
     let conn = db::connect(&spec)?;
-    let ms = get_migrations()?;
+    let ms = get_migrations(base_dir)?;
     let unapplied = ms.unapplied_migrations(&conn)?;
     let all = ms.all_migrations()?;
     for m in all {
@@ -193,15 +197,15 @@ pub fn list_migrations() -> Result<()> {
     Ok(())
 }
 
-pub fn collapse_migrations(new_initial_name: Option<&String>) -> Result<()> {
+pub fn collapse_migrations(base_dir: &PathBuf, new_initial_name: Option<&String>) -> Result<()> {
     let name = match new_initial_name {
         Some(name) => format!("{}_{}", default_name(), name),
         None => default_name(),
     };
-    let spec = load_connspec()?;
+    let spec = load_connspec(base_dir)?;
     let backend = spec.get_backend()?;
     let conn = db::connect(&spec)?;
-    let mut ms = get_migrations()?;
+    let mut ms = get_migrations(base_dir)?;
     let latest = ms.last_applied_migration(&conn)?;
     if latest.is_none() {
         eprintln!("There are no migrations to collapse");
@@ -212,26 +216,26 @@ pub fn collapse_migrations(new_initial_name: Option<&String>) -> Result<()> {
     ms.create_migration_to(&backend, &name, None, latest_db)?;
     let new_migration = ms.latest().unwrap();
     new_migration.mark_applied(&conn)?;
-    let cli_state = CliState::load()?;
+    let cli_state = CliState::load(base_dir)?;
     if cli_state.embedded {
         // Update the embedding
-        embed()?;
+        embed(base_dir)?;
     }
     println!("Collapsed all changes into new single migration '{name}'");
     Ok(())
 }
 
-pub fn delete_table(name: &str) -> Result<()> {
-    let mut ms = get_migrations()?;
+pub fn delete_table(base_dir: &Path, name: &str) -> Result<()> {
+    let mut ms = get_migrations(base_dir)?;
     let current = ms.current();
     current.delete_table(name)?;
     Ok(())
 }
 
-pub fn clear_data() -> Result<()> {
-    let spec = load_connspec()?;
+pub fn clear_data(base_dir: &PathBuf) -> Result<()> {
+    let spec = load_connspec(base_dir)?;
     let conn = db::connect(&spec)?;
-    let latest = match get_migrations()?.last_applied_migration(&conn)? {
+    let latest = match get_migrations(base_dir)?.last_applied_migration(&conn)? {
         Some(m) => m,
         None => {
             eprintln!("No migrations have been applied, so no data is recognized.");
@@ -245,13 +249,13 @@ pub fn clear_data() -> Result<()> {
     Ok(())
 }
 
-pub fn clean() -> Result<()> {
-    get_migrations()?.clear_current()?;
+pub fn clean(base_dir: &Path) -> Result<()> {
+    get_migrations(base_dir)?.clear_current()?;
     Ok(())
 }
 
-pub fn get_migrations() -> Result<FsMigrations> {
-    let root = base_dir()?.join("migrations");
+pub fn get_migrations(base_dir: &Path) -> Result<FsMigrations> {
+    let root = base_dir.join("migrations");
     if !root.exists() {
         eprintln!("No butane migrations directory found. Add at least one model to your project and build.");
         std::process::exit(1);

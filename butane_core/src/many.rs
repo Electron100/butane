05c7108c7836a4ec708ed4efc7538017bb1a7fed
John Vandenberg <jayvdb@gmail.com>
2023-07-28 22:22:07 +0800

    Add Many.load_ordered (#131)
    Loads the values of the `Many` sorted. Accomplished through new `query`/`load_query` methods which `load` is also rewritten in terms of. 
    
    These other new methods are not public at this time as they have some limitations that may not be obvious from the methods signature, but we may make them public in the future if the need for other similar use cases arises.

--- butane_core/src/many.rs
+++ butane_core/src/many.rs
@@ -1,7 +1,7 @@
 //! Implementation of many-to-many relationships between models.
 #![deny(missing_docs)]
 use crate::db::{Column, ConnectionMethods};
-use crate::query::{BoolExpr, Expr};
+use crate::query::{BoolExpr, Expr, OrderDirection, Query};
 use crate::{DataObject, Error, FieldType, Result, SqlType, SqlVal, ToSql};
 use once_cell::unsync::OnceCell;
 use serde::{Deserialize, Serialize};
@@ -128,20 +128,40 @@ where
     /// Loads the values referred to by this many relationship from the
     /// database if necessary and returns a reference to them.
     pub fn load(&self, conn: &impl ConnectionMethods) -> Result<impl Iterator<Item = &T>> {
+        let query = self.query();
+        // If not initialised then there are no values
+        let vals: Result<Vec<&T>> = if query.is_err() {
+            Ok(Vec::new())
+        } else {
+            Ok(self.load_query(conn, query.unwrap())?.collect())
+        };
+        vals.map(|v| v.into_iter())
+    }
+
+    /// Query the values referred to by this many relationship from the
+    /// database if necessary and returns a reference to them.
+    fn query(&self) -> Result<Query<T>> {
+        let owner: &SqlVal = match &self.owner {
+            Some(o) => o,
+            None => return Err(Error::NotInitialized),
+        };
+        Ok(T::query().filter(BoolExpr::Subquery {
+            col: T::PKCOL,
+            tbl2: self.item_table.clone(),
+            tbl2_col: "has",
+            expr: Box::new(BoolExpr::Eq("owner", Expr::Val(owner.clone()))),
+        }))
+    }
+
+    /// Loads the values referred to by this many relationship from a
+    /// database query if necessary and returns a reference to them.
+    fn load_query(
+        &self,
+        conn: &impl ConnectionMethods,
+        query: Query<T>,
+    ) -> Result<impl Iterator<Item = &T>> {
         let vals: Result<&Vec<T>> = self.all_values.get_or_try_init(|| {
-            //if we don't have an owner then there are no values
-            let owner: &SqlVal = match &self.owner {
-                Some(o) => o,
-                None => return Ok(Vec::new()),
-            };
-            let mut vals = T::query()
-                .filter(BoolExpr::Subquery {
-                    col: T::PKCOL,
-                    tbl2: self.item_table.clone(),
-                    tbl2_col: "has",
-                    expr: Box::new(BoolExpr::Eq("owner", Expr::Val(owner.clone()))),
-                })
-                .load(conn)?;
+            let mut vals = query.load(conn)?;
             // Now add in the values for things not saved to the db yet
             if !self.new_values.is_empty() {
                 vals.append(
@@ -153,6 +173,25 @@ where
             Ok(vals)
         });
         vals.map(|v| v.iter())
+    }
+
+    /// Loads and orders the values referred to by this many relationship from a
+    /// database if necessary and returns a reference to them.
+    pub fn load_ordered(
+        &self,
+        conn: &impl ConnectionMethods,
+        order: OrderDirection,
+    ) -> Result<impl Iterator<Item = &T>> {
+        let query = self.query();
+        // If not initialised then there are no values
+        let vals: Result<Vec<&T>> = if query.is_err() {
+            Ok(Vec::new())
+        } else {
+            Ok(self
+                .load_query(conn, query.unwrap().order(T::PKCOL, order))?
+                .collect())
+        };
+        vals.map(|v| v.into_iter())
     }
 
     /// Describes the columns of the Many table

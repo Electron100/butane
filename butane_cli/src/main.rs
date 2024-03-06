521207af1c6b9c7ea144223d2e4fa71b0fa4ebc5
John Vandenberg <jayvdb@gmail.com>
2024-03-06 11:21:21 +0800

    Use clap derive (#197)
    Provides better compile-time errors

--- butane_cli/src/main.rs
+++ butane_cli/src/main.rs
@@ -2,62 +2,34 @@ use std::path::PathBuf;
 
 use butane_cli::{
     base_dir, clean, clear_data, collapse_migrations, delete_table, detach_latest_migration, embed,
-    get_migrations, handle_error, list_migrations, migrate, Result,
+    get_migrations, handle_error, init, list_migrations, make_migration, migrate, Result,
 };
-use clap::{value_parser, Arg, ArgMatches, ArgAction};
+use clap::{ArgAction, Parser, Subcommand};
 
-fn main() {
-    let app = clap::Command::new("butane")
-        .version(env!("CARGO_PKG_VERSION"))
-        .author("James Oakley <james@electronstudio.org>")
-        .about("Manages butane database migrations")
-        .subcommand_required(true)
-        .max_term_width(80)
-        .arg(
-            Arg::new("path").short('p').long("path")
-            .default_value(base_dir().into_os_string())
-            .value_parser(value_parser!(PathBuf))
-            .help("Select directory to locate butane state")
-        )
-        .subcommand(
-            clap::Command::new("init")
-                .about("Initialize the database")
-                .arg(
-                    Arg::new("BACKEND")
-                        .required(true)
-                        .index(1)
-                        .help("Database backend to use. 'sqlite' or 'pg'"),
-                )
-                .arg(
-                    Arg::new("CONNECTION")
-                        .required(true)
-                        .index(2)
-                        .help("Database connection string. Format depends on backend"),
-                )
-                .arg(
-                    Arg::new("connect")
-                    .long("no-connect")
-                    .action(ArgAction::SetFalse)
-                    .required(false)
-                    .num_args(0)
-                    .help("Do not connect to the database"),
-                ),
-        )
-        .subcommand(
-            clap::Command::new("makemigration")
-                .about("Create a new migration")
-                .arg(
-                    Arg::new("NAME")
-                        .required(true)
-                        .index(1)
-                        .help("Name to use for the migration"),
-                ),
-        )
-        .subcommand(
-            clap::Command::new("detach-migration")
-                .about("Detach the latest migration")
-                .alias("detachmigration")
-                .after_help(r#"This command removes the latest migration from the list of migrations and sets butane state to before the latest migration was created.
+#[derive(Parser)]
+#[command(author, version, about = "Manages butane database migrations.")]
+#[command(propagate_version = true, max_term_width = 80)]
+struct Cli {
+    #[command(subcommand)]
+    command: Commands,
+    #[arg(short = 'p', long, default_value=base_dir().into_os_string())]
+    path: PathBuf,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    /// Initialize the database.
+    Init(InitCommand),
+    /// Create a new migration.
+    #[command(alias = "makemigration")]
+    MakeMigration {
+        /// Name to use for the migration.
+        name: String,
+    },
+    /// Detach the latest migration.
+    #[command(
+        alias = "detachmigration",
+        after_help = "This command removes the latest migration from the list of migrations and sets butane state to before the latest migration was created.
 
 The removed migration is not deleted from file system.
 
@@ -66,57 +38,69 @@ This operation is the first step of the process of rebasing a migration onto oth
 If the migration has not been manually edited, it can be automatically regenerated after being rebased. In this case, deleting the detached migration is often the best approach.
 
 However if the migration has been manually edited, it will need to be manually re-attached to the target migration series after the rebase has been completed.
-"#
-                )
-        )
-        .subcommand(clap::Command::new("migrate").about("Apply migrations"))
-        .subcommand(clap::Command::new("list").about("List migrations"))
-        .subcommand(clap::Command::new("collapse").about("Replace all migrations with a single migration representing the current model state.").arg(
-            Arg::new("NAME")
-                .required(true)
-                .index(1)
-                .help("Name to use for the new migration"),
-        ))
-        .subcommand(
-            clap::Command::new("embed").about("Embed migrations in the source code"),
-        )
-        .subcommand(
-            clap::Command::new("rollback")
-                .about("Rollback migrations. With no arguments, undoes the latest migration. If the name of a migration is specified, rolls back until that migration is the latest applied migration")
-                .arg(
-                    Arg::new("NAME")
-                        .required(false)
-                        .index(1)
-                        .help("Migration to roll back to"),
-                ),
-        )
-        .subcommand(
-            clap::Command::new("clear")
-                .arg_required_else_help(true)
-                .about("Clear data")
-                .subcommand(clap::Command::new("data")
-                    .about("Clear all data from the database. The schema is left intact, but all instances of all models (i.e. all rows of all tables defined by the models) are deleted")))
-        .subcommand(
-            clap::Command::new("delete")
-                .about("Delete a table")
-                .arg_required_else_help(true)
-                .subcommand(
-                    clap::Command::new("table")
-                        .about("Delete a table. Deleting a model in code does not currently lead to deletion of the table.")
-                        .arg(
-                            Arg::new("TABLE")
-                                .required(true)
-                                .index(1)
-                                .help("Name of table to delete"),
-                        ),
-                ),
-        )
-        .subcommand(
-            clap::Command::new("clean")
-                .about("Clean current migration state. Deletes the current migration working state which is generated on each build. This can be used as a workaround to remove stale tables from the schema, as Butane does not currently auto-detect model removals. The next build will recreate with only tables for the extant models."))
-                .arg_required_else_help(true);
-    let args = app.get_matches();
-    let mut base_dir = args.get_one::<PathBuf>("path").unwrap().clone();
+"
+    )]
+    DetachMigration,
+    /// Apply migrations.
+    Migrate,
+    /// List migrations.
+    List,
+    /// Replace all migrations with a single migration representing the current model state.
+    Collapse {
+        /// Name to use for the new migration.
+        name: String,
+    },
+    /// Embed migrations in the source code.
+    Embed,
+    /// Rollback migrations. With no arguments, undoes the latest migration. If the name of a migration is specified, rolls back until that migration is the latest applied migration.
+    Rollback {
+        /// Migration to roll back to.
+        name: String,
+    },
+    /// Clear.
+    Clear {
+        #[clap(subcommand)]
+        subcommand: ClearCommands,
+    },
+    /// Delete.
+    Delete {
+        #[clap(subcommand)]
+        subcommand: DeleteCommands,
+    },
+    /// Clean current migration state. Deletes the current migration working state which is generated on each build. This can be used as a workaround to remove stale tables from the schema, as Butane does not currently auto-detect model removals. The next build will recreate with only tables for the extant models.
+    Clean,
+}
+
+#[derive(Parser)]
+struct InitCommand {
+    /// Database connection string. Format depends on backend.
+    backend: String,
+    /// Database backend to use. 'sqlite' or 'pg'.
+    connection: String,
+    /// Do not connect to the database.
+    #[arg(required = false, long="no-connect", action = ArgAction::SetFalse)]
+    connect: bool,
+}
+
+#[derive(Subcommand)]
+enum ClearCommands {
+    /// Clear all data from the database. The schema is left intact, but all instances of all models (i.e. all rows of all tables defined by the models) are deleted.
+    Data,
+}
+
+#[derive(Subcommand)]
+enum DeleteCommands {
+    /// Clear all data from the database. The schema is left intact, but all instances of all models (i.e. all rows of all tables defined by the models) are deleted.
+    Table {
+        /// Table name.
+        name: String,
+    },
+}
+
+fn main() {
+    let cli = Cli::parse();
+
+    let mut base_dir = cli.path;
     base_dir.push(".butane");
 
     // List any detached migrations.
@@ -133,53 +117,35 @@ However if the migration has been manually edited, it will need to be manually r
         };
     };
 
-    match args.subcommand() {
-        Some(("init", sub_args)) => handle_error(init(&base_dir, Some(sub_args))),
-        Some(("makemigration", sub_args)) => {
-            handle_error(make_migration(&base_dir, Some(sub_args)))
-        }
-        Some(("detach-migration", _)) => handle_error(detach_latest_migration(&base_dir)),
-        Some(("migrate", _)) => handle_error(migrate(&base_dir)),
-        Some(("rollback", sub_args)) => handle_error(rollback(&base_dir, Some(sub_args))),
-        Some(("embed", _)) => handle_error(embed(&base_dir)),
-        Some(("list", _)) => handle_error(list_migrations(&base_dir)),
-        Some(("collapse", sub_args)) => {
-            handle_error(collapse_migrations(&base_dir, sub_args.get_one("NAME")))
-        }
-        Some(("clear", sub_args)) => match sub_args.subcommand() {
-            Some(("data", _)) => handle_error(clear_data(&base_dir)),
-            _ => eprintln!("Unknown clear command. Try: clear data"),
+    match &cli.command {
+        Commands::Init(args) => handle_error(init(
+            &base_dir,
+            &args.backend,
+            &args.connection,
+            args.connect,
+        )),
+        Commands::MakeMigration { name } => handle_error(make_migration(&base_dir, Some(name))),
+        Commands::DetachMigration => handle_error(detach_latest_migration(&base_dir)),
+        Commands::Migrate => handle_error(migrate(&base_dir)),
+        Commands::Rollback { name } => handle_error(rollback(&base_dir, Some(name))),
+        Commands::Embed => handle_error(embed(&base_dir)),
+        Commands::List => handle_error(list_migrations(&base_dir)),
+        Commands::Collapse { name } => handle_error(collapse_migrations(&base_dir, Some(name))),
+        Commands::Clear { subcommand } => match subcommand {
+            ClearCommands::Data => handle_error(clear_data(&base_dir)),
         },
-        Some(("delete", sub_args)) => match sub_args.subcommand() {
-            Some(("table", sub_args2)) => handle_error(delete_table(
-                &base_dir,
-                sub_args2.get_one::<&str>("TABLE").unwrap(),
-            )),
-            _ => eprintln!("Unknown delete command. Try: delete table"),
+        Commands::Delete { subcommand } => match subcommand {
+            DeleteCommands::Table { name } => handle_error(delete_table(&base_dir, name)),
         },
-        Some(("clean", _)) => handle_error(clean(&base_dir)),
-        Some((_, _)) | None => panic!("Unreachable as clap handles this automatically"),
+        Commands::Clean => handle_error(clean(&base_dir)),
     }
 }
 
-fn init(base_dir: &PathBuf, args: Option<&ArgMatches>) -> Result<()> {
-    let args = args.unwrap();
-    let name: &String = args.get_one("BACKEND").unwrap();
-    let connstr: &String = args.get_one("CONNECTION").unwrap();
-    let connect: bool = *args.get_one::<bool>("connect").unwrap();
-    butane_cli::init(base_dir, name, connstr, connect)
-}
-
-fn make_migration(base_dir: &PathBuf, args: Option<&ArgMatches>) -> Result<()> {
-    let name_arg = args.and_then(|a| a.get_one::<String>("NAME"));
-    butane_cli::make_migration(base_dir, name_arg)
-}
-
-fn rollback(base_dir: &PathBuf, args: Option<&ArgMatches>) -> Result<()> {
+fn rollback(base_dir: &PathBuf, name: Option<&String>) -> Result<()> {
     let spec = butane_cli::load_connspec(base_dir)?;
     let conn = butane::db::connect(&spec)?;
 
-    match args.and_then(|a| a.get_one::<String>("NAME")) {
+    match name {
         Some(to) => butane_cli::rollback_to(base_dir, conn, to),
         None => butane_cli::rollback_latest(base_dir, conn),
     }

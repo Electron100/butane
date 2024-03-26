68c57d91b020d1dd56dfeabef507aef397e8f5df
John Vandenberg <jayvdb@gmail.com>
2024-03-26 15:49:32 +0800

    Add data to test; skip on sqlite

--- examples/getting_started/tests/rollback.rs
+++ examples/getting_started/tests/rollback.rs
@@ -1,6 +1,38 @@
-use butane::db::Connection;
+use butane::db::{BackendConnection, Connection};
 use butane::migrations::{Migration, Migrations};
+use butane::DataObject;
 use butane_test_helper::*;
+
+use getting_started::models::{Blog, Post, Tag};
+
+fn create_tag(connection: &Connection, name: &str) -> Tag {
+    let mut tag = Tag::new(name);
+    tag.save(connection).unwrap();
+    tag
+}
+
+fn insert_data(connection: &Connection) {
+    if connection.backend_name() == "sqlite" {
+        // https://github.com/Electron100/butane/issues/226
+        return;
+    }
+    let mut cats_blog = Blog::new("Cats");
+    cats_blog.save(connection).unwrap();
+
+    let tag_asia = create_tag(connection, "asia");
+    let tag_danger = create_tag(connection, "danger");
+
+    let mut post = Post::new(
+        &cats_blog,
+        "The Tiger".to_string(),
+        "The tiger is a cat which would very much like to eat you.".to_string(),
+    );
+    post.published = true;
+    post.likes = 4;
+    post.tags.add(&tag_danger).unwrap();
+    post.tags.add(&tag_asia).unwrap();
+    post.save(connection).unwrap();
+}
 
 fn migrate_and_rollback(mut connection: Connection) {
     // Migrate forward.
@@ -8,15 +40,19 @@ fn migrate_and_rollback(mut connection: Connection) {
     let migrations = butane_cli::get_migrations(&base_dir).unwrap();
     let to_apply = migrations.unapplied_migrations(&connection).unwrap();
     for migration in &to_apply {
-        migration.apply(&mut connection).unwrap();
+        migration
+            .apply(&mut connection)
+            .unwrap_or_else(|err| panic!("migration {} failed: {err}", migration.name()));
         eprintln!("Applied {}", migration.name());
     }
 
-    // TODO: Insert data
+    insert_data(&connection);
 
     // Rollback migrations.
     for migration in to_apply.iter().rev() {
-        migration.downgrade(&mut connection).unwrap();
+        migration
+            .downgrade(&mut connection)
+            .unwrap_or_else(|err| panic!("rollback of {} failed: {err}", migration.name()));
         eprintln!("Rolled back {}", migration.name());
     }
 }

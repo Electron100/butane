ba679f38a6b008b2cb64def671b6e4fe7533e6bf
John Vandenberg <jayvdb@gmail.com>
2025-04-07 15:27:49 +0800

    Fix clippy::doc_overindented_list_items

--- butane_core/src/db/mod.rs
+++ butane_core/src/db/mod.rs
@@ -3,14 +3,14 @@
 //! The different ways of referring to a database handle may present
 //! some initial confusion.
 //! * `ConnectionMethods` is a trait containing the methods available on a database connection or a transaction.
-//!    Most methods on a [DataObject][crate::DataObject] or a [Query][crate::query::Query] require an
-//!    implementation of `ConnectionMethods`.
+//!   Most methods on a [DataObject][crate::DataObject] or a [Query][crate::query::Query] require an
+//!   implementation of `ConnectionMethods`.
 //! * `BackendConnection` is a trait representing a direct connection to a database backend. It is a superset
 //!   of `ConnectionMethods` and also includes the ability to create a transaction.
 //! * `Transaction` is a struct representing a database transaction. It implements `ConnectionMethods`.
 //! * `Connection` is a convenience struct containing a boxed `BackendConnection`. It cannot do anything other than
-//!    what a `BackendConnection` can do, but allows using a single concrete type that is not tied to a particular
-//!    database backend. It is returned by the `connect` method.
+//!   what a `BackendConnection` can do, but allows using a single concrete type that is not tied to a particular
+//!   database backend. It is returned by the `connect` method.
 
 #![allow(missing_docs)]
 

1103498a76b620430e3d7e0ef75ce54979801177
James Oakley <james@electronstudio.org>
2025-04-13 15:44:29 -0400

    migrations

---
+++ examples/custom_pg/src/butane_migrations.rs
@@ -0,0 +1,22 @@
+//! Butane migrations embedded in Rust.
+
+use butane::migrations::MemMigrations;
+
+/// Load the butane migrations embedded in Rust.
+pub fn get_migrations() -> Result<MemMigrations, butane::Error> {
+    let json = r#"{
+  "migrations": {},
+  "current": {
+    "name": "current",
+    "db": {
+      "tables": {},
+      "extra_types": {}
+    },
+    "from": null,
+    "up": {},
+    "down": {}
+  },
+  "latest": null
+}"#;
+    MemMigrations::from_json(json)
+}

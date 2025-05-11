86a080a2987f42a96e894a9448f1b8bb499f675c
James Oakley <james@electronstudio.org>
2025-05-10 20:41:57 -0400

    is_in

--- butane/tests/query.rs
+++ butane/tests/query.rs
@@ -281,7 +281,7 @@ async fn offset(conn: ConnectionAsync) {
 #[butane_test]
 async fn query_in(conn: ConnectionAsync) {
     blog::setup_blog(&conn).await;
-    let mut posts = query!(Post, title.inn("The Tiger", "Sir Charles")).load(&conn).await.unwrap();
+    let mut posts = query!(Post, title.is_in("The Tiger", "Sir Charles")).load(&conn).await.unwrap();
     assert_eq!(posts.len(), 2);
     posts.sort_by(|p1, p2| p1.id.partial_cmp(&p2.id).unwrap());
     assert_eq!(posts[0].title, "The Tiger");

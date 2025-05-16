afbb2f6d1008871c17f0388c0736f3090fa1db3d
John Vandenberg <jayvdb@gmail.com>
2025-05-16 17:11:44 +0800

    Fix cleaning .butane when running tests

--- butane/build.rs
+++ butane/build.rs
@@ -1,16 +1,22 @@
 fn main() {
-    // This build.rs is only needed to help the tests
-    if std::env::var("CARGO_CFG_TEST").is_err() {
-        return;
-    }
-    println!("cargo:rerun-if-changed=tests");
+    println!("cargo:rerun-if-changed=tests/");
 
-    // This cleans the .butane/ directory which is generated when compiling the tests so that
-    // the tests do not encounter side effects from previous test runs.
-    // Currently the only way to remove stale items from the generated .butane/ directory is to
-    // delete it before the code is compiled.
-    // This means we can not rely on `butane clean` or the code behind it, because it hasnt
-    // been compiled yet.
+    // Note that #[cfg(test)] and CARGO_CFG_TEST do not work in build.rs
+    // See https://github.com/rust-lang/cargo/issues/4789
+    #[cfg(feature = "_auto_delete_dot_butane")]
+    {
+        clean_dot_butane();
+    }
+}
+
+/// This cleans the .butane/ directory which is generated when compiling the tests so that
+/// the tests do not encounter side effects from previous test runs.
+/// Currently the only way to remove stale items from the generated .butane/ directory is to
+/// delete it before the code is compiled.
+/// This means we can not rely on `butane clean` or the code behind it, because it hasnt
+/// been compiled yet.
+#[cfg(feature = "_auto_delete_dot_butane")]
+fn clean_dot_butane() {
     let dir = ".butane/";
     if std::path::Path::new(&dir).is_dir() {
         println!("cargo:warning=Deleting .butane directory");
